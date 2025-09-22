a4599c25b570574dad2f352655d3c2d21134b289
prachiijainn <prachiijainn1@gmail.com>
2025-09-22 12:03:00 +0530

    gradle settings changed

--- metro.config.js
+++ metro.config.js
@@ -6,6 +6,22 @@ const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
  *
  * @type {import('@react-native/metro-config').MetroConfig}
  */
-const config = {};
+const config = {
+  transformer: {
+    // Optimize bundle size and speed
+    minifierConfig: {
+      // Faster minification
+      keep_fnames: true,
+    },
+  },
+  resolver: {
+    // Blacklist problematic modules that slow down bundling
+    blockList: [
+      /node_modules\/.*\/Pods\/.*/,
+      /.*\/__tests__\/.*/,
+    ],
+  },
+  watchFolders: [],
+};
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
